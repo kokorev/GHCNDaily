7f7006c8aa2caa727110e698e24970e7742264d4
Vasily Kokorev <kokorev@users.noreply.github.com>
2020-02-03 21:25:26 +0100

    Add files via upload

---
+++ GHCNdaily/__init__.py
@@ -0,0 +1,6 @@
+from .ghcn import Inventory, Meta, read_dly, download_data
+
+__author__ = 'V. Kokorev'
+__email__ = "v.kokorev@utwente.nl"
+
+__all__ = ['Inventory', 'Meta', 'read_dly', 'download_data', '__author__', '__email__']
